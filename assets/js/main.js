228001f185ae11b98ad6a345756310664328ca61
evmcodi <emaccuarta@protonmail.com>
2019-11-06 01:30:44 +0000

    Added fade in page animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@
 // Responsive Hamburger menu javascript from https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    var x = document.getElementById("myTopnavmob");
+    if (x.className === "topnavmob") {
       x.className += " responsive";
     } else {
-      x.className = "topnav";
+      x.className = "topnavmob";
     }
   } 
